21ca2142b52bdf42a60ac47ad66769af36fdb522
amrith-am <amm.amrith@gmail.com>
2023-05-01 13:42:15 +0530

    change commit

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -4,7 +4,7 @@ import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { pinecone } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
 /* Name of directory to retrieve your files from */
 const filePath = 'docs';
@@ -12,29 +12,31 @@ const filePath = 'docs';
 export const run = async () => {
   try {
     /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new CustomPDFLoader(path),
-    });
+    const loader = new PDFLoader("docs/anatomy.pdf");
+    const docs = await loader.load();
+    // const directoryLoader = new DirectoryLoader(filePath, {
+    //   '.pdf': (path) => new CustomPDFLoader(path),
+    // });
 
-    // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
+    // // const loader = new PDFLoader(filePath);
+    // const rawDocs = await directoryLoader.load();
 
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 2000,
-      chunkOverlap: 200,
-    });
+    // /* Split text into chunks */
+    // const textSplitter = new RecursiveCharacterTextSplitter({
+    //   chunkSize: 2000,
+    //   chunkOverlap: 200,
+    // });
 
-    const docs = await textSplitter.splitDocuments(rawDocs);
+    // const docs = await textSplitter.splitDocuments(rawDocs);
     console.log('split docs', docs);
-    let newList = [];
+    // let newList = [];
 
-    // iterate through each document in the list and append a string
-    for (let i = 0; i < docs.length; i++) {
-      let updatedDoc = docs[i] + "source - .pdf";
-      // add the updated document to the new list
-      newList.push(updatedDoc);
-    }
+    // // iterate through each document in the list and append a string
+    // for (let i = 0; i < docs.length; i++) {
+    //   let updatedDoc = docs[i] + "source - .pdf";
+    //   // add the updated document to the new list
+    //   newList.push(updatedDoc);
+    // }
 
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
@@ -42,7 +44,7 @@ export const run = async () => {
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
     //embed the PDF documents
-    await PineconeStore.fromDocuments(newList, embeddings, {
+    await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
       namespace: PINECONE_NAME_SPACE,
       textKey: 'text',
