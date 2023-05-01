44743acbc9befa6c78936c3280caefbc5fe32385
amrith-am <amm.amrith@gmail.com>
2023-05-01 14:13:19 +0530

    change commit

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -10,7 +10,8 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-The context are got it from different documents so 
+The context have Source pdf also. please make answers related to the source context.
+We can have one or more Answers. make answers with Source mentioned.
 If you don't know the answer, just say you don't know.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
