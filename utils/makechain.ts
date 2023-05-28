5df6490a060a85d8fc392c1dd5004ad835a3d15b
Amrith <61175452+amrith-am@users.noreply.github.com>
2023-05-28 15:54:04 +0530

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,8 +9,8 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-The context have Source pdf also. please make answers related to the source context.
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of content to answer the question at the end.
+The content have Source pdf also. please make answers related to the source context.
 We can have one or more Answers. make answers with Source mentioned.
 If you don't know the answer, just say you don't know.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
