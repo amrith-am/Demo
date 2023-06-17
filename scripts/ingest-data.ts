4a5f0e6c64e5001896e37d9db4642114a3af26e1
Amrith <61175452+amrith-am@users.noreply.github.com>
2023-06-17 09:02:32 +0530

    Update ingest-data.ts

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -5,11 +5,6 @@ import { pinecone } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
-// import { OpenAI } from 'langchain';
-// import { VectorDBQA } from 'langchain/chains';
-// import { RetrievalQA } from 'langchain/chains';
-import { PagedPDFSplitter } from 'langchain/document_loaders';
-
 
 /* Name of directory to retrieve your files from */
 const filePath = 'docs';
@@ -22,37 +17,16 @@ export const run = async () => {
     });
 
     // const loader = new PDFLoader(filePath);
-//     const rawDocs = await directoryLoader.load();
-//     import * as fs from 'fs';
-
-    const directoryPath = '/content/docs';
-
-    const fileNames: string[] = fs.readdirSync(directoryPath);
-
-    const docs: string[] = [];
-
-    for (let i = 0; i < fileNames.length; i++) {
-      const bookPath = `${directoryPath}/${fileNames[i]}`;
-      console.log(i);
-      console.log(bookPath);
-      const loader = new PagedPDFSplitter(bookPath);
-      docs.push(...loader.loadAndSplit());
-    }
-
+    const rawDocs = await directoryLoader.load();
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1500,
+      chunkSize: 1000,
       chunkOverlap: 200,
     });
 
     const docs = await textSplitter.splitDocuments(rawDocs);
     console.log('split docs', docs);
-
-    // iterate through each document in the list and append a string
-//     for (let i = 0; i < docs.length; i++) {
-//       docs[i].pageContent += '\nSource: ' + docs[i].metadata.source;
-//     }
 
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
