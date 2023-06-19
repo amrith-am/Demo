8cd8772e302fdd89a6ad15786958d660c1afafa8
Amrith <61175452+amrith-am@users.noreply.github.com>
2023-06-19 15:31:20 +0530

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,15 +9,15 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of content to answer the question at the end.
-The content have Source pdf also. please make answers related to the source context.
-We can have one or more Answers. make answers with Source mentioned.
+const QA_PROMPT = `You are a helpful AI assistant for selecting suitable insurance policy for user need. Use the following pieces of PAGE_CONTEXT to answer the question at the end.
+The PAGE_CONTEXT have POLICY_NAME also. please make answers related to the PAGE_CONTEXT.
+for USER_SITUATION provide suitable POLICY_NAME with explanation.
 If you don't know the answer, just say you don't know.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
 
-Question: {question}
+USER_SITUATION: {question}
 Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
