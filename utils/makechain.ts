7e2361b379a439ace57b0837f6112b7c30049bc0
Amrith <61175452+amrith-am@users.noreply.github.com>
2023-06-20 10:53:08 +0530

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,10 +2,8 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT = `Given the following question, rephrase the follow up question to be a standalone question, the question will used to retrieve the related context from a vector store.
 
-Chat History:
-{chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
